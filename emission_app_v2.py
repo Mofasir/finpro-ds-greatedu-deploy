0380aac1aa73b4f6d00a53e59dafe5b806e04f27
Mohammad Faikar Natsir <misteralfikri@gmail.com>
2024-06-08 23:11:03 +0700

    Update emission_app_v2.py
    Update till "home"

--- emission_app_v2.py
+++ emission_app_v2.py
@@ -1,7 +1,10 @@
 import os
+import io
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
+from PIL import Image
+import plotly.express as px
 import numpy as np
 import pandas as pd
 
@@ -12,12 +15,15 @@ st.set_page_config(
     page_icon="🌍"
 )
 
+# Getting the working directory of the emission_app.py
+    working_dir = os.path.dirname(os.path.abspath(__file__))
+
 # Sidebar menu
 with st.sidebar:
     selected = option_menu(
         menu_title="Main Menu",
-        options=["Home", "Predict Emissions", "About"],
-        icons=["house", "graph-up", "info-circle"],
+        options=["Home", "Data Description", "Analytics","Predict Emissions", "About"],
+        icons=["house", "clipboard", "graph-up", "cloud", "people"],
         menu_icon="cast",
         default_index=0,
     )
@@ -38,6 +44,157 @@ def sin_cos_encode(latitude, longitude, week_no, month, max_week_val):
 
 # Home Page
 if selected == "Home":
+    # Load the image background
+    image_path = os.path.join(working_dir, "images/background.png")
+    image = Image.open(image_path)
+    buffered = io.BytesIO()
+    image.save(buffered, format="PNG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+
+    # Load the image logo
+    image_path = os.path.join(working_dir, "images/logo.png")
+    image = Image.open(image_path)
+    buffered = io.BytesIO()
+    image.save(buffered, format="PNG")
+    img_str1 = base64.b64encode(buffered.getvalue()).decode()
+
+    st.markdown(
+        f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/jpeg;base64,{img_str}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+        }}
+        .header {{
+            display: flex;
+            justify-content: space-between;
+        }}  
+        .logo {{
+            display: flex;
+            background-position: center;
+            margin-top: 20px;
+            background-image: url("data:image/jpeg;base64,{img_str1}");
+            background-size: contain;
+            background-repeat: no-repeat;
+            width: auto;
+            height: auto;
+        }}
+        .main-content {{
+            display: flex;
+            justify-content: space-between;
+            border-radius: 10px;
+            margin-top: 60px;
+        }}
+        .header .title h1 {{
+        }}
+        .fotodas{{
+            display: flex;
+            justify-content: space-between;
+        }}
+        .des{{
+            flex: 1;
+            margin-left: 10px;
+            text-align: justify;
+        }}
+        .description {{
+            border-radius: 10px;
+            margin-top: 20 px;
+        }}
+        .objective {{
+            flex: 1;
+            padding: 20px;
+            border-radius: 10px;
+            background-color: #D9FAEE;
+            margin-right: 10px;
+            width: 50%;
+        }}
+        .benefit {{
+            flex: 1;
+            padding: 20px;
+            border-radius: 10px;
+            background-color: #D9FAEE;
+            width: 50%;
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    st.markdown(
+    """
+        <div class="header">
+            <div class="title">
+                <h1>Predict CO2 Emissions in Rwanda</h1>
+            </div>
+            <div class="kumlogo"></div>
+        </div>
+        <hr style="margin: 20px 0; border-top: 1px solid #ddd;">
+        <div class="main-content">
+            <div class="description">
+              <div class = "fotodas">
+                <div class ="air"></div>
+                <p class = "des">Pentingnya kualitas air dalam menjaga keberlanjutan lingkungan dan kesehatan masyarakat telah menjadi perhatian utama di seluruh dunia. 
+                Air adalah aset berharga yang tidak dapat digantikan, namun sering kali terpapar oleh berbagai faktor, mulai dari polusi industri hingga limbah domestik. 
+                Dalam menghadapi tantangan ini, perlu adanya upaya untuk mengembangkan sistem prediktif yang mampu memantau dan mengukur kualitas air secara akurat. 
+                Melalui analisis data dan teknologi yang inovatif, kita dapat mengidentifikasi pola perilaku air dan memprediksi potensi risiko yang terkait dengan perubahan lingkungan.</p>
+              </div>
+                <p style="text-align: justify;">Pada Studi Kasus Water Quality Prediction bertujuan untuk mengatasi tantangan ini dengan mengembangkan model prediksi yang efektif dan dapat diandalkan. 
+                Dengan memanfaatkan data terkini dan teknik analisis yang canggih, kita dapat memperkirakan kualitas air di lokasi tertentu dan mengidentifikasi faktor-faktor yang berpotensi mempengaruhi. 
+                Dengan demikian, upaya ini tidak hanya akan membantu dalam menjaga keberlanjutan sumber daya air, tetapi juga dapat memberikan informasi yang berharga bagi pengambil keputusan dalam menangani masalah lingkungan di masa depan.</p>
+                <div style='display: flex; justify-content: space-between; margin-bottom: 20px;'>
+                  <div class="objective">
+                    <h6 style="text-align: center;">Objective</h6>
+                    <p style="font-size: 0.8em; text-align: justify;">
+                        <span><b>1. Meningkatkan pemahaman tentang kualitas air</b></span><br>
+                        Melalui analisis prediktif, tujuan utama adalah meningkatkan pemahaman tentang faktor-faktor yang memengaruhi kualitas air di berbagai lokasi. Hal ini akan membantu dalam mengidentifikasi sumber polusi dan potensi risiko terhadap kesehatan manusia dan lingkungan.<br>
+                        <span><b>2. Peningkatan responsibilitas lingkungan</b></span><br>
+                        Dengan memprediksi kualitas air secara akurat, tujuan ini adalah untuk memberikan solusi yang dapat meningkatkan tanggung jawab lingkungan dalam pengelolaan sumber daya air. Hal ini dapat mencakup upaya untuk mengurangi polusi air, mengoptimalkan penggunaan air, dan meminimalkan dampak negatif terhadap ekosistem air.<br>
+                        <span><b>3. Mendukung keberlanjutan lingkungan</b></span><br>
+                        Melalui pemahaman yang lebih baik tentang kualitas air, tujuan ini adalah untuk mendukung upaya-upaya dalam menjaga keberlanjutan lingkungan. Hal ini dapat mencakup pengelolaan air yang lebih efisien, perlindungan habitat air, dan pemulihan ekosistem air yang terganggu.<br>
+                        <span><b>4. Peningkatan kesehatan masyarakat</b></span><br>
+                        Dengan memantau kualitas air secara berkala dan melakukan prediksi yang akurat, tujuan ini adalah untuk melindungi kesehatan masyarakat dari risiko yang berkaitan dengan konsumsi air yang tercemar. Hal ini akan membantu dalam mengurangi risiko penyakit terkait air dan meningkatkan kualitas hidup masyarakat secara keseluruhan.
+                    </p>
+                  </div>
+                </div>
+            </div>
+        </div>
+    """, 
+    unsafe_allow_html=True
+    )
+    
+    # Footer
+    st.markdown("""
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <div style="display: flex; justify-content: space-between; align-items: flex-start; margin-top: 40px; border-top: 1px solid #ddd; padding-top: 10px;">
+        <div style="display: flex; align-items: flex-start; font-size: 15px;">
+            <i class="material-icons" style="font-size: 25px; margin-right: 5px; color: #4C4D50 ;">location_on</i>
+            <div style="font-size: 15px; color: #4C4D50;">
+                Jl. Duren Tiga Raya No.09, RT.12/RW.1, Duren<br>
+                Tiga, Kec. Pancoran, Kota Jakarta Selatan,<br>
+                Daerah Khusus Ibukota Jakarta 12760
+            </div>
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+    
+    st.markdown("""
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <div style="display: flex; flex-direction: column;">
+        <hr style="border-top: 1px solid #ddd; margin: 10px 0;">
+        <div style="display: flex; justify-content: space-between; align-items: center; margin-top: 5px;">
+            <div style="display: flex; align-items: center; font-size: 15px; color: #4C4D50;">
+                <i class="far fa-copyright" style="font-size: 20px; margin-right: 5px;"></i>
+                2024 <span style="margin-left: 2px"><b>Fun-tastic Four</b></span>. All Rights Reserved
+            </div>
+            <div style="font-size: 14px; margin: 0; color: #4C4D50;">
+                SIB Cycle 6 | 2024
+            </div>
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
+    
     st.title('Welcome to the CO2 Emission Prediction App')
     st.markdown("""
     This application predicts CO2 emissions using machine learning models trained on open-source emissions data.
